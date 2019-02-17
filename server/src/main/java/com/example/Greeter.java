e22104bd052dbf542995a91b6263f766795207e2
Tercio Almeida <tercio.almeida@paysmart.com.br>
2019-02-17 11:20:28 -0300

    Fixed codestyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,10 @@ public class Greeter {
 
   }
 
-  /** 
-   * The glorious greet method
+  /**
+   * The glorious greet method.
+   * @param someone who is going to be greeted.
+   * @returns greeting string.
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
